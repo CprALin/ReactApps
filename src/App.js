c8fc6245fd37d8d7b47bf5608f8ac67deb7d3443
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-08 15:42:33 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+
+function Steps({steps , addStep , decStep}) {
+
+    return(
+       <div className="steps">
+            <button className="steps-btn" onClick={decStep}>-</button>
+              <p>{`Step : ${steps}`}</p>
+            <button className="steps-btn" onClick={addStep}>+</button>
+       </div>
+    );
+}
+
+function Count({ steps ,count , addCount , decCount}){
+
+    return(
+      <div className="steps">
+          <button className="steps-btn" onClick={decCount}>-</button>
+            <p>{`Count : ${count}`}</p>
+          <button className="steps-btn" onClick={addCount}>+</button>
+      </div>
+    );
+}
+
+function Content({count}){
+    const date = new Date("February 8 2024");
+    date.setDate(date.getDate() + count);
+
+    return(
+      <div>
+         <span>
+            {
+              count === 0 ? (
+                  "Today is "
+              ) : count > 0 ? (
+                  `${count} days from today is `
+              ) : (
+                  `${Math.abs(count)} days ago was `
+              )
+            }
+         </span>
+         <span>{date.toDateString()}</span>
+      </div>
+    );
+}
 
 function App() {
+  const[steps , setSteps] = useState(1);
+  const[count , setCount] = useState(0);
+
+
+  function handleAddStep(){
+    setSteps((s) => s + 1);
+  }
+
+  function handleDecStep(){
+    if(steps > 1)
+    {
+      setSteps((s) => s - 1);
+    }  
+  }
+
+  function handleAddCount(){
+    setCount((s) => s + steps );
+  }
+
+  function handleDecCount(){
+    setCount((s) => s - steps);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+      <Steps steps={steps} addStep={handleAddStep} decStep={handleDecStep}/>
+      <Count steps={steps} count={count} addCount={handleAddCount} decCount={handleDecCount}/>
+      <Content count={count}/>
+   </div>
   );
 }
 
