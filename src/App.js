40409141fb363a5e9054c4018102ba28c8c5918b
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-08 13:20:46 +0200

    -

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const messages = [
 ];
 
 export default function App(){
-    const [step , setStep] = useState(1)
+    const [step , setStep] = useState(1);
+    const [isOpen , setIsOpen] = useState(true);
     
     //console.log(arr);
 
@@ -28,23 +29,29 @@ export default function App(){
     }
 
     return(
-      <div className="steps">
-        <div className="numbers">
-            <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-            <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-            <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
-        </div>
+      <div>
+        <button className='close' onClick={() => setIsOpen(!isOpen)}>&times;</button>
 
-        <p className="message">Step {step} : {messages[step - 1]}</p>
+        {isOpen && (
+          <div className="steps">
+            <div className="numbers">
+                <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+                <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+                <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+            </div>
 
-        <div className="buttons">
-            <button style={{backgroundColor : "#7950f2" , color : "#fff"}} onClick={handlePrevious}>
-              Previos
-            </button>
-            <button style={{backgroundColor : "#7950f2" , color : "#fff"}} onClick={handleNext}>
-              Next
-            </button>
-        </div>
+            <p className="message">Step {step} : {messages[step - 1]}</p>
+
+            <div className="buttons">
+                <button style={{backgroundColor : "#7950f2" , color : "#fff"}} onClick={handlePrevious}>
+                  Previos
+                </button>
+                <button style={{backgroundColor : "#7950f2" , color : "#fff"}} onClick={handleNext}>
+                  Next
+                </button>
+            </div>
+          </div>
+        )}  
       </div>
     );
 }
