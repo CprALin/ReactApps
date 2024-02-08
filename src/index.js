0acc30ea6aa63bfba19b537fe6185a8b0745eb35
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-08 11:22:27 +0200

    -

--- src/index.js
+++ src/index.js
@@ -102,13 +102,12 @@ function Footer(){
         alert("Sorry we're closed.")
     } */
 
+    //if(!isOpen) return <p>Message</p>
+
     return(
         <footer className="footer">
            {isOpen ? (
-              <div className="order">
-                <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-                <button className="btn">Order</button>
-              </div>    
+                <Order closeHour={closeHour}/>
             ) : (<p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>)}
         </footer>
         
@@ -116,14 +115,27 @@ function Footer(){
     );
 }
 
-function Pizza(props){
+function Order({closeHour}){
+    return(
+      <div className="order">
+        <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+        <button className="btn">Order</button>
+      </div>   
+    );
+}
+
+
+function Pizza({pizzaObject}){
+    
+    if(pizzaObject.soldOut) return null;
+  
     return(
        <li className="pizza">
-          <img src={props.pizzaObject.photoName} alt={props.pizzaObject.name}/>
+          <img src={pizzaObject.photoName} alt={pizzaObject.name}/>
           <div>
-            <h3>{props.pizzaObject.name}</h3>
-            <p>{props.pizzaObject.ingredients}</p>
-            <span>{props.pizzaObject.price}</span>
+            <h3>{pizzaObject.name}</h3>
+            <p>{pizzaObject.ingredients}</p>
+            <span>{pizzaObject.price}</span>
           </div>
        </li> 
     );
