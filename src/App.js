c3813dd13a37ad334de13032bb99b0b8b718c233
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-09 08:26:25 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Logo from "./logo";
+import { PackingList } from "./PackingList";
+import Stats from "./stats";
+import { Form } from "./Form";
 
 /* const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -40,123 +44,6 @@ function App() {
         <Stats items={items}/>  
      </div>
   );
-}
-
-function Logo(){
-   return(
-      <h1>🌴Far Away👜</h1>
-   );
-}
-
-function Form({onAddItems}){
-  const [description , setDescription] = useState('');
-  const [quantity , setQuantity] = useState(1); 
-
-  function handleSubmit(e){
-      e.preventDefault();
-
-      if(!description) return;
-
-      const newItem = { description , quantity , packed : false , id : Date.now()};
-
-      onAddItems(newItem);
-
-      setDescription("");
-      setQuantity(1); 
-   }
-
-   return(
-      <form className="add-form" onSubmit={handleSubmit}>
-          <h3>What do you need for your 🤔 trip?</h3>
-          <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-              {Array.from({length : 20} , (_ , i) => i + 1).map
-              (num => (
-                  <option value={num} key={num}>
-                      {num}
-                  </option>
-              ))}
-          </select>
-          <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
-          <button>Add</button>
-      </form>
-   );
-}
-
-function PackingList({items , onDeleteItem , onToggleItem , onClearList}){
-    const [sortBy , setSortBy] = useState('input');
-
-    let sortedItems;
-
-    if(sortBy === 'input')
-    {
-      sortedItems = items;
-    }
-
-    if(sortBy === 'description')
-    {
-      sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
-    }
-
-    if(sortBy === 'packed')
-    {
-       sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
-    }
-
-    return(
-      <div className="list">
-        <ul>
-            {sortedItems.map(item => <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>)}
-        </ul>
-
-        <div className="actions">
-            <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
-                <option value='input'>Sort by input order</option>
-                <option value='description'>Sort by description</option>
-                <option value='packed'>Sort by packed status</option>
-            </select>
-            <button onClick={onClearList}>Clear list</button>
-        </div>
-      </div>
-    );
-}
-
-function Stats({items}){
-    if(!items.length)
-    {
-      return(
-         <footer className="stats">
-           <em>
-              Start adding some items to your packing list 📃
-           </em>
-         </footer>
-      );
-    }
-
-    const numItems = items.length;
-    const numPacked = items.filter((item) => item.packed).length;
-    const percentage = Math.round((numPacked / numItems) * 100);
-  
-    return(
-       <footer className="stats">
-          <em>
-            {
-              percentage === 100 ? 'You got everything! Ready to go ✈️' : `👜 You have ${numItems} items on your list , and you already packed ${numPacked} (${percentage}%)`
-            }
-          </em>
-       </footer>
-    );
-}
-
-function Item({item , onDeleteItem , onToggleItem}){
-    return(
-      <li>
-          <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)}/>
-          <span style={item.packed ? {textDecoration : "line-through"} : {}}>
-             {item.quantity} {item.description} 
-          </span>
-          <button onClick={() => onDeleteItem(item.id)}>❌</button>
-      </li>
-    );
 }
 
 export default App;
---
+++ src/logo.js
@@ -0,0 +1,8 @@
+
+function Logo(){
+    return(
+       <h1>🌴Far Away👜</h1>
+    );
+ }
+
+ export default Logo;
