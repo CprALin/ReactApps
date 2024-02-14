053220650a0c8725165b2d6fa01e93268e2ba15c
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-14 17:42:19 +0200

    -

--- currency-converter-app/src/App.js
+++ currency-converter-app/src/App.js
@@ -1,25 +1,51 @@
 
+// https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD
+
+import { useEffect, useState } from "react";
+
 function App() {
+  const [amount , setAmount] = useState(1);
+  const [fromCur , setFromCur] = useState('EUR');
+  const [toCur , setToCur] = useState('USD');
+  const [converted , setConverted] = useState('');
+  const [isLoading , setIsLoading] = useState(false);
+
+  useEffect(function() {
+    async function convert(){
+        setIsLoading(true);
+        const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`);
+        const data = await res.json();
+        setConverted(data.rates[toCur]);
+        setIsLoading(false);
+    }
+
+    if(fromCur === toCur) return setConverted(amount);
+
+    convert();
+  }, [amount , fromCur , toCur]);
+
   return (
     <div className="App">
         
-        <input type="text" />
+        <input type="text" value={amount} onChange={(e) => setAmount(Number(e.target.value))} disabled={isLoading}/>
         
-        <select className="select">
-            <option value='USD'>USD</option>
-            <option value='EUR'>EUR</option>
-            <option value='CAD'>CAD</option>
-            <option value='INR'>INR</option>
-        </select>
-        
-        <select className="select">
-            <option value='USD'>USD</option>
-            <option value='EUR'>EUR</option>
-            <option value='CAD'>CAD</option>
-            <option value='INR'>INR</option>
-        </select>
+        <div className="selectDiv">
+          <select className="select" value={fromCur} onChange={(e) => setFromCur(e.target.value)} disabled={isLoading}>
+              <option value='USD'>USD</option>
+              <option value='EUR'>EUR</option>
+              <option value='CAD'>CAD</option>
+              <option value='INR'>INR</option>
+          </select>
+          
+          <select className="select" value={toCur} onChange={(e) => setToCur(e.target.value)} disabled={isLoading}>
+              <option value='USD'>USD</option>
+              <option value='EUR'>EUR</option>
+              <option value='CAD'>CAD</option>
+              <option value='INR'>INR</option>
+          </select>
+        </div>
 
-        <p>OUTPUT</p>
+        <p>{converted} {toCur}</p>
     </div>
   );
 }
