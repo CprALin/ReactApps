2cb2ad1b8ec8b58cc46d73d0c17ce7c9c02c3a81
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-14 16:45:18 +0200

    -

---
+++ currency-converter-app/src/App.js
@@ -0,0 +1,27 @@
+
+function App() {
+  return (
+    <div className="App">
+        
+        <input type="text" />
+        
+        <select className="select">
+            <option value='USD'>USD</option>
+            <option value='EUR'>EUR</option>
+            <option value='CAD'>CAD</option>
+            <option value='INR'>INR</option>
+        </select>
+        
+        <select className="select">
+            <option value='USD'>USD</option>
+            <option value='EUR'>EUR</option>
+            <option value='CAD'>CAD</option>
+            <option value='INR'>INR</option>
+        </select>
+
+        <p>OUTPUT</p>
+    </div>
+  );
+}
+
+export default App;
